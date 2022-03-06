ae958d6ebf1da8cd1fddcd1ed590449999e3480b
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-03-06 15:00:29 +0530

    cover

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -3,7 +3,11 @@ import "./top.css";
 export default function top() {
   return (
     <div className="top">
-      <img src="https://i.ibb.co/dQSNp7N/SMImage.jpg" alt="" />
+      <h1>Welcome to ICTAK Student Enrollment</h1>
+      <img
+        src="https://i.ibb.co/x5KDbBw/Timeline-Covers-pro-black-laptop-computer-keyboard-facebook-cover.jpg"
+        alt=""
+      />
     </div>
   );
 }
